ddac71ad21f6753c03b388ccd62c34aae3526acf
connieyeeson <connieyeeson@gmail.com>
2023-06-06 17:09:53 +0000

    assignment

---
+++ answer.js
@@ -0,0 +1,93 @@
+
+ // 1. declare an array with 5 items.
+
+    let arr = [ "Plum", "Orange", "Palm", "Fish", "Grape"];
+
+ // 2. add an item to the array
+
+    arr[5] = "pig";
+
+ // 3. update the third item in the array
+
+        arr[2] = "oil";
+
+ // 4. let arr = [0,1,2,3,4,[5,6,7],8,9]
+ 
+  let arr = [0,1,2,3,4,[5,6,7],8,9];
+
+ //  5.  print 5 from the array define above
+
+  arr [5] [0];
+
+ //   6. print the last item in the array above using the array length property
+  
+  arr [7];
+  
+ //   write a program that swaps 0 and 1 in the array and produces this output [1,0,2,3,4,[5,6,7],8,9]*/ 
+
+
+ // 8. declear an object call class with 3 properties
+
+ let chort3 = {
+    name: "", 
+    age: "", 
+    favSubject: "", 
+ };
+
+ //  9. add a new property to the object
+
+ chort3["gender"]= ""
+
+
+ // 13.let people = [
+ //     14.    {name: "mary", contact: 0881999000},
+ //     15.    {name: "paul", contact: 0881999600},
+ //     16.    {name: "peter", contact: 0881999400},
+ //     17.    {name: "sara", contact: 0881999300}
+ //     18.]
+
+ let people = [
+
+  {name: "mary", contact: 0881999000},
+  {name: "paul", contact: 0881999600},
+  {name: "peter", contact: 0881999400},
+  {name: "sara", contact: 0881999300}
+        
+ ];
+
+    // 19. print mary from the list of people 
+
+    people[0].name;
+
+    // 20. add gender to each of the object in the array
+
+    people[0]['gender'] = "female";
+    people[1]['gender'] = "male";
+    people[2]['gender'] = "male";
+    people[3]['gender'] = "female";
+    people[4]['gender'] = "female";
+
+
+
+    // 21. add a new object to the list of people
+
+    people[4] = {name:"Connie", contact: 775561490};
+
+    // 22. print sara's contact
+
+    people[3].contact
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
